4b8dc22780882863ab372b2d0b173385e8aa0d50
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-12-26 10:31:07 +0000

    Create data folders that get lost when cloning

--- {{cookiecutter.project_slug}}/make.sh
+++ {{cookiecutter.project_slug}}/make.sh
@@ -13,7 +13,35 @@ create_env () {
     pipenv install --dev
 }
 
+create_folders() {
+    if [ ! -d data ]
+    then
+        mkdir data
+    fi
+    if [ ! -d data/external ]
+    then
+        mkdir data/external
+    fi
+    if [ ! -d data/interim ]
+    then
+        mkdir data/interim
+    fi
+    if [ ! -d data/processed ]
+    then
+        mkdir data/processed
+    fi
+    if [ ! -d data/raw ]
+    then
+        mkdir data/raw
+    fi
+    if [ ! -d bin ]
+    then
+        mkdir bin
+    fi
+}
+
 setup () {
+    create_folders
     create_env
 }
 
